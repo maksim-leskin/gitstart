9089ac019c71bf95f273a604e83feaae8e016ffc
ЬфлMaksim Leskin <maksim.leskin@methed.ru>
2022-04-08 16:05:52 +0300

    day2

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,13 +1,6 @@
-/******/ (() => { // webpackBootstrap
+/******/ (function() { // webpackBootstrap
 /******/ 	"use strict";
 var __webpack_exports__ = {};
 
-;// CONCATENATED MODULE: ./src/js/modules/test.js
-var sum = function sum(a, b) {
-  return a + b;
-};
-;// CONCATENATED MODULE: ./src/js/index.js
-
-document.write(sum(3, 6));
 /******/ })()
 ;
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,1 @@
-import {sum} from './modules/test.js';
 
-document.write(sum(3,6))
