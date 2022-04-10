daa612bb019c2d5ffe737d5a401ba83c0ecd43ae
ЬфлMaksim Leskin <maksim.leskin@methed.ru>
2022-04-10 14:48:07 +0300

    day3

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,39 @@
+import Navigo from 'navigo';
 
+const library = document.querySelector('.library');
+const book = document.querySelector('.book');
+const add = document.querySelector('.add');
+const addBtns = document.querySelectorAll('.header__btn-add, .library__add-btn');
+const backBtn = document.querySelector('.header__btn_back');
+
+
+const router = new Navigo('/', {
+  hash: true,
+});
+
+const closeAllPage = () => {
+  library.classList.add('hidden');
+  book.classList.add('hidden');
+  add.classList.add('hidden');
+}
+
+router.on({
+  '/': () => {
+    closeAllPage();
+    library.classList.remove('hidden');
+  },
+  'book': () => {
+    closeAllPage();
+    book.classList.remove('hidden');
+  },
+  'add' : () => {
+    closeAllPage();
+    add.classList.remove('hidden');
+  }
+}).resolve();
+
+addBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    router.navigate('add');
+  })
+});
