e188ac117c9b6ec238630a805723ffa878de5359
ЬфлMaksim Leskin <maksim.leskin@methed.ru>
2022-04-12 13:02:28 +0300

    day4

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,9 @@ const library = document.querySelector('.library');
 const book = document.querySelector('.book');
 const add = document.querySelector('.add');
 const addBtns = document.querySelectorAll('.header__btn-add, .library__add-btn');
-const backBtn = document.querySelector('.header__btn_back');
+const backBtn = document.querySelectorAll('.header__btn_back');
+const btnSearch = document.querySelectorAll('.header__btn_search');
+const search = document.querySelector('.search');
 
 
 const router = new Navigo('/', {
@@ -36,4 +38,25 @@ addBtns.forEach(btn => {
   btn.addEventListener('click', () => {
     router.navigate('add');
   })
+});
+
+backBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    router.navigate('/');
+  })
+});
+
+const closeSearch = ({target}) => {
+  if (target.closest('.search, .header__btn_search')) {
+    return;
+  }
+  search.classList.remove('search_active');
+  document.body.removeEventListener('click', closeSearch);
+};
+
+btnSearch.forEach(btn => {
+  btn.addEventListener('click', () => {
+    search.classList.add('search_active');
+    document.body.addEventListener('click', closeSearch);
+  });
 });
