2d941167628a7e6b93cb3db46cef6cd4c3d182d3
ЬфлMaksim Leskin <maksim.leskin@methed.ru>
2022-04-16 18:56:01 +0300

    day6

---
+++ src/js/modules/serviceBook.js
@@ -0,0 +1,22 @@
+export const API_URI =  'http://localhost:3024/';
+
+export const getBooks = async (id) => {
+  const response = await fetch(`${API_URI}api/books/${id || ''}`);
+
+  if (response.ok) {
+    return response.json();
+  }
+
+  throw new Error(response.statusText);
+}
+
+
+export const getLabels = async () => {
+  const response = await fetch(`${API_URI}api/label/`);
+
+  if (response.ok) {
+    return response.json();
+  }
+
+  throw new Error(response.statusText);
+}
